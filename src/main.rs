d4f6703c13b0af900c91fea953d949913301fea9
Paul van der Meijs <vandermeijs@redkiwi.nl>
2023-11-27 19:30:09 +0100

    Challenge -1

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{http::StatusCode, routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(hello_world)).route(
+        "/-1/error",
+        get(|| async { (StatusCode::INTERNAL_SERVER_ERROR, "") }),
+    );
+
+    Ok(router.into())
+}
